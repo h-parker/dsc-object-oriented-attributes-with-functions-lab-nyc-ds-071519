049e2c1f483d30d5f3945fed45a3cf8f27f35c77
Hannah Parker <hparker5@u.rochester.edu>
2019-07-18 16:08:02 -0400

    work

--- school.py
+++ school.py
@@ -1,1 +1,24 @@
 class School:
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+        
+        
+    def add_student(self, name, grade): 
+        if self.roster.get(grade)!= None: # if grade not None(thus exists)
+            self.roster[grade].append(name) # add student to vals
+        
+        else: # otherwise
+            self.roster[grade] = [name] # make new key, add student to list
+        
+        return
+    
+    
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    
+    def sort_roster(self):
+        for grade in self.roster:
+            self.roster[grade].sort()
+        return self.roster
